5c750532ba28797515500fa5a9ffa97257215cd5
Michael Vega <ec3572h@comcel.com.co>
2021-09-18 16:59:16 -0500

    Creacion de proyecto
    Crear el esqueleto del proyecto maven

---
+++ src/main/java/co/com/micha3l/vega/generator/ms/GeneratorMsApplication.java
@@ -0,0 +1,13 @@
+package co.com.micha3l.vega.generator.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorMsApplication.class, args);
+	}
+
+}
