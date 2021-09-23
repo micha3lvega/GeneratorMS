373bda223062c01bef5ddd0ce5ded90aa3138a3a
Michael Vega <ec3572h@comcel.com.co>
2021-09-23 14:22:24 -0500

    Crear template pom

---
+++ src/main/java/co/com/micha3l/vega/generator/ms/template/Pom.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package co.com.micha3l.vega.generator.ms.template;
+
+/**
+ * Clase que representa el archivo pom de los templates que se usan para generar
+ * el archivo
+ *
+ * @author micha3lvega
+ *
+ */
+public class Pom extends Template {
+
+	public Pom() {
+		super("pom.txt");
+	}
+
+}
